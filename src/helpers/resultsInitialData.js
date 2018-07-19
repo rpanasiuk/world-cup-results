1d964058c517a30a4e777e616d4febddc2dd07e3
Rafał Panasiuk <panasiukraf@gmail.com>
2018-07-19 16:05:30 +0200

    Added teams data for one of the group stage groups.

---
+++ src/helpers/resultsInitialData.js
@@ -0,0 +1,22 @@
+const groups = [
+	[
+		{
+			name: "Poland",
+			ISO: "POL"
+		},
+		{
+			name: "Brazil",
+			ISO: "BRA"
+		},
+		{
+			name: "Germany",
+			ISO: "GER"
+		},
+		{
+			name: "Spain",
+			ISO: "ESP"
+		}
+	]
+];
+
+export default groups;
